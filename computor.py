921c3c06857d065dc5320702178bf07a9627ce92
Tamas Tokesi <ttokesi@2-D-7.42heilbronn.de>
2024-08-02 21:54:29 +0200

    changing for texting. fuction to get coificience

--- computor.py
+++ computor.py
@@ -1,6 +1,30 @@
 print("Welcome to computor!")
 print("Usage. (e.g. a * X^0 + b * X^1 - c * X^2 = d * X^0)")
-equation_input = input('Enter the polynomial equation:')
+#equation_input = input('Enter the polynomial equation:')
 
 print("--- ---- SOLUTION ---- ---")
-print(equation_input)
+
+def findpower(part, power):
+    """Searches for the power parts and returns them"""
+    num = part.split(f"*X^{power}")
+    return num[0]
+
+def parsel_the_equation(equation):
+    """Parse the polynomial equation in the specified format and return the coefficients a, b, and c."""
+    # Remove spaces
+    equation = equation.replace(' ', '')
+    
+    # Split the equation into LHS and RHS
+    lhs, rhs = equation.split('=')
+
+
+
+    print("lhs: ", lhs)
+    print("rhs", rhs)
+
+    print("powers: ", findpower(lhs, 0))
+
+
+parsel_the_equation("5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0")
+#parsel_the_equation(equation_input)
+# print(equation_input)
