5efe55ffe8fe6dd439332fbff13a4b8e75e08773
Tamas Tokesi <ttokesi@2-D-7.42heilbronn.de>
2024-08-02 20:16:20 +0200

    get input, print back input

--- computor.py
+++ computor.py
@@ -1,1 +1,6 @@
 print("Welcome to computor!")
+print("Usage. (e.g. a * X^0 + b * X^1 - c * X^2 = d * X^0)")
+equation_input = input('Enter the polynomial equation:')
+
+print("--- ---- SOLUTION ---- ---")
+print(equation_input)
